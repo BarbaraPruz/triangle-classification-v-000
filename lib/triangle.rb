87fc1e37a4c7ac3e6ce0e6625e05a8b79fbfb0c9
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-06 21:54:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
-  
+
   def initialize ( s1, s2, s3 )
     @side1 = s1
     @side2 = s2
@@ -11,6 +11,12 @@ class Triangle
     (side1>0 && side2>0 && side3>0) && (side1+side2>side3 && side2+side3>side1 && side1+side3>side2)
   end
 
+  def equilateral?
+    side1==side2 && side2==side3
+  end
+  def isosceles?
+    side1==side2  || side2==side3 || side1==side3
+  end  
   def kind
     rtn_kind = nil
     if !valid?
@@ -19,18 +25,18 @@ class Triangle
       rescue PartnerError => error
         puts error.message
       end
-    else if side1==side2 && side2==side3
+    else equilateral? 
       rtn_kind = :equilateral
-    else if side1==side2  || side2==side3 || side1==side3
-      rtn_kind = :isoscles
-    else  
+    else isosceles?
+      rtn_kind = :isosceles
+    else
       rtn_kind = :scalene
     end
     rtn_kind
-  end  
+  end
 
   class PartnerError < StandardError
-    def message 
+    def message
       "you must give the get_married method an argument of an instance of the person class!"
     end
   end
